fd4a2261ba6cdcc6e500b58ef6482fc994d6fa1b
Jack Frate <jack@noEmail.com>
2019-12-02 22:33:17 -0500

    updated the js crackhead example

--- js/order/example.js
+++ js/order/example.js
@@ -2,6 +2,9 @@ const second = 1000;
 
 // print 1 after 1 second
 function print1() {
+  // this is an 'anonymous function'
+  // its just a function without a name
+  // set timeout makes us pass in a function
   setTimeout(() => { console.log('1') }, second);
 }
 
